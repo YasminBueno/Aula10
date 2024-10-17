0cc31413b5cf6dce68d4a883b6269b859853e500
Yasmin Bueno <yasminbbueno@gmail.com>
2024-10-17 12:35:42 -0300

    1710

--- escola/src/componentes/Rotas.jsx
+++ escola/src/componentes/Rotas.jsx
@@ -10,6 +10,15 @@ import Erro from "../paginas/Erro";
 
 export default function Rotas() {
     return(
-        
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/cadastro" element={<Cadastro/>}/>
+                <Route path="/consulta" element={<Consulta/>}/>
+                <Route path="/alteracao/:codigo" element={<Alteracao/>}/>
+                <Route path="/exclusao/:codigo" element={<Exclusao/>}/>
+                <Route path="*" element={<Erro/>}/>
+            </Routes>
+        </BrowserRouter>
     ); 
 }
