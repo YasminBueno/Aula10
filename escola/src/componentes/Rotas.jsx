dc64a5c4ef044a2b20302bd691ba59a993581a67
Yasmin Bueno <yasminbbueno@gmail.com>
2024-10-17 12:26:11 -0300

    1710

--- escola/src/componentes/Rotas.jsx
+++ escola/src/componentes/Rotas.jsx
@@ -1,8 +1,15 @@
 
+import {BrowserRouter, Routes,Route} from "react-router-dom";
+
+import Home from "../paginas/Home";
+import Cadastro from "../paginas/Cadastro";
+import Consulta from "../paginas/Consulta";
+import Alteracao from "../paginas/Alteracao";
+import Exclusao from "../paginas/Exclusao";
+import Erro from "../paginas/Erro"; 
+
 export default function Rotas() {
     return(
-        <div>
-            <h1>Rotas</h1>
-        </div>
+        
     ); 
 }
--- escola/src/paginas/Home.jsx
+++ escola/src/paginas/Home.jsx
@@ -2,7 +2,7 @@
 export default function Home() {
     return(
         <div>
-            <h1>Home</h1>
+            <h1>Aula 10 - Projeto Escola com MySQL</h1>
         </div>
     ); 
 }
